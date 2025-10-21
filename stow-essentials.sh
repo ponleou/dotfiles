916e5b5a39bfd9e932b07678c24b585a3740dc80
Keo Ponleou Sok <ponleousk@gmail.com>
2025-10-21 17:29:50 +1100

    autocommit: change in /home/ponleou/.dotfiles/

--- stow-essentials.sh
+++ stow-essentials.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 script_dir="$(dirname "$(realpath "$0")")" # directory of where the script is
-stow --dir=$script_dir/essentials --target=$HOME easyeffects GIMP mpv omz cliphist dolphin sway vesktop
+stow --dir=$script_dir/essentials --target=$HOME easyeffects GIMP mpv omz cliphist dolphin sway vesktop zen
