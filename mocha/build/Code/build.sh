2ee1084c57ac23d1014458c5d94f0c57f53cd1a2
Keo Ponleou Sok <ponleousk@gmail.com>
2025-10-27 16:23:03 +1100

    autocommit: change in /home/ponleou/.dotfiles/mocha/build/Code/

--- mocha/build/Code/build.sh
+++ mocha/build/Code/build.sh
@@ -5,7 +5,7 @@
 script_dir="$(dirname "$(realpath "$0")")" # directory of where the script is
 
 # save static settings.json to .build
-static=sed "s/\"workbench.colorTheme\": \"[^\"]*\"/\"workbench.colorTheme\": \"\"/; s/\"catppuccin.accentColor\": \"[^\"]*\"/\"catppuccin.accentColor\": \"\"/" "$script_dir/settings.json"
+static=$(sed "s/\"workbench.colorTheme\": \"[^\"]*\"/\"workbench.colorTheme\": \"\"/; s/\"catppuccin.accentColor\": \"[^\"]*\"/\"catppuccin.accentColor\": \"\"/" "$script_dir/settings.json")
 echo "$static" > "$script_dir/settings.json.build"
 echo "$static" > "$script_dir/settings.json"
 
