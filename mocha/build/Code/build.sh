44ae900a4f5217fe3e60ffd1fe5aecd9f3e531a5
Keo Ponleou Sok <ponleousk@gmail.com>
2025-10-27 15:33:43 +1100

    autocommit: change in /home/ponleou/.dotfiles/mocha/build/Code/

--- mocha/build/Code/build.sh
+++ mocha/build/Code/build.sh
@@ -3,4 +3,3 @@
 script_dir="$(dirname "$(realpath "$0")")" # directory of where the script is
 sed -i "s/\"workbench.colorTheme\": \"[^\"]*\"/\"workbench.colorTheme\": \"Catppuccin Mcha\"/" "$script_dir/settings.json"
 sed -i "s/\"catppuccin.accentColor\": \"[^\"]*\"/\"catppuccin.accentColor\": \"$accent\"/" "$script_dir/settings.json"
-echo $script_dir
