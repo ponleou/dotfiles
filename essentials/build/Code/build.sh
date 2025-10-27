7545ee0efcb931c3d8483b9fdc2ef9b24154ed14
Keo Ponleou Sok <ponleousk@gmail.com>
2025-10-27 18:19:28 +1100

    autocommit: change in /home/ponleou/.dotfiles/essentials/build/Code/

--- essentials/build/Code/build.sh
+++ essentials/build/Code/build.sh
@@ -3,3 +3,5 @@
 script_dir="$(dirname "$(realpath "$0")")" # directory of where the script is
 
 cat $script_dir/settings.json.build > $script_dir/settings.json
+
+ln -sf $script_dir/settings_build_hook.sh $script_dir/../../../stow-scripts/hooks
