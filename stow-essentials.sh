3a7d22b66c7c3ac7860bf2c0d475e4c0acb0ef4b
Keo Ponleou Sok <ponleousk@gmail.com>
2025-10-27 16:32:13 +1100

    autocommit: change in /home/ponleou/.dotfiles/

--- stow-essentials.sh
+++ stow-essentials.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 script_dir="$(dirname "$(realpath "$0")")" # directory of where the script is
+
+build() {
+  for package in "$@"; do
+    export expbuild_accent=$accent
+    bash "$script_dir/essentials/build/$package/build.sh"
+  done
+}
+
 stow --dir=$script_dir/essentials --target=$HOME easyeffects GIMP mpv omz cliphist dolphin sway vesktop zen Code
+build Code
