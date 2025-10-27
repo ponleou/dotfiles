2830338d8ab658ee2cd49d0c40ec18f3e74643bf
Keo Ponleou Sok <ponleousk@gmail.com>
2025-10-27 15:45:36 +1100

    autocommit: change in /home/ponleou/.dotfiles/mocha/build/Code/

--- mocha/build/Code/build.sh
+++ mocha/build/Code/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# variables with "expbuild_" prefix are exported variables when called
+# variables with "expbuild_" prefix are exported variables by the build function when called
 
 script_dir="$(dirname "$(realpath "$0")")" # directory of where the script is
 sed -i --follow-symlinks "s/\"workbench.colorTheme\": \"[^\"]*\"/\"workbench.colorTheme\": \"Catppuccin Mocha\"/" $script_dir/settings.json
