3cd8f59cd8e1cae7e1da1fdfcd2c3658afe6fec0
Keo Ponleou Sok <ponleousk@gmail.com>
2025-11-03 17:03:43 +1100

    autocommit: change in /home/ponleou/.dotfiles/essentials/sway/.config/sway/scripts/

--- essentials/sway/.config/sway/scripts/wallpaper-rofi.sh
+++ essentials/sway/.config/sway/scripts/wallpaper-rofi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 script_dir="$(dirname "$(realpath "$0")")" # directory of where the script is
-
-cp $1 $script_dir/../wallpapers/
-
+wallpapers_dir="$script_dir/../wallpapers/"
 filename=$(basename $1)
+
+cp $1 $wallpapers_dir
 echo "output * bg wallpapers/$filename fill" > $script_dir/../wallpaper
