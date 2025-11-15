586f47bc4b1bd2abcf86b223a840b54c4c4c6b37
Keo Ponleou Sok <ponleousk@gmail.com>
2025-11-15 15:52:40 +1100

    autocommit: change in /home/ponleou/.dotfiles/essentials/build/Code/

--- essentials/build/Code/settings_build_hook.sh
+++ essentials/build/Code/settings_build_hook.sh
@@ -3,4 +3,4 @@
 script_dir="$(dirname "$(realpath "$0")")" # directory of where the script is
 
 # save static settings.json to .build
-jq '."workbench.colorTheme" = "" | ."catppuccin.accentColor" = ""' "$script_dir/settings.json" > "$script_dir/settings.json.build"
+# jq '."workbench.colorTheme" = "" | ."catppuccin.accentColor" = ""' "$script_dir/settings.json" > "$script_dir/settings.json.build"
