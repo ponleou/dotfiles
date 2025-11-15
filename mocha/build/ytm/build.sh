af77794efcee81e4eec34658f003d85168f07e5a
Keo Ponleou Sok <ponleousk@gmail.com>
2025-11-15 16:44:01 +1100

    autocommit: change in /home/ponleou/.dotfiles/mocha/build/ytm/

--- mocha/build/ytm/build.sh
+++ mocha/build/ytm/build.sh
@@ -3,4 +3,4 @@
 # variables with "expbuild_" prefix are exported variables by the build function when called
 
 script_dir="$(dirname "$(realpath "$0")")" # directory of where the script is
-jq '.options.themes = ["'"$HOME"'/.config/YouTube Music/mocha.css"]' "$script_dir/config.json"
+jq '.options.themes = ["'"$HOME"'/.config/YouTube Music/mocha.css"]' "$script_dir/config.json" | sponge "$script_dir/config.json"
