7159c89107a36220aac893cdff4712675b6f4947
Keo Ponleou Sok <ponleousk@gmail.com>
2025-11-15 15:25:11 +1100

    autocommit: change in /home/ponleou/.dotfiles/essentials/build/ytm/

--- essentials/build/ytm/build.sh
+++ essentials/build/ytm/build.sh
@@ -2,7 +2,7 @@
 
 script_dir="$(dirname "$(realpath "$0")")" # directory of where the script is
 
-cat $script_dir/settings.json.build > $script_dir/settings.json
+cat $script_dir/config.json.build > $script_dir/config.json
 
 
 ln -sf $script_dir/config_build_hook.sh $script_dir/../../../stow-scripts/hooks/ytm_build_essentials.sh
