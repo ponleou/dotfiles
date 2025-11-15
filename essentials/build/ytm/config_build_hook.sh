1a81b0d137b946c433f87ad142ddd1d1bf60267d
Keo Ponleou Sok <ponleousk@gmail.com>
2025-11-15 15:54:13 +1100

    autocommit: change in /home/ponleou/.dotfiles/essentials/build/ytm/

--- essentials/build/ytm/config_build_hook.sh
+++ essentials/build/ytm/config_build_hook.sh
@@ -2,4 +2,4 @@
 
 script_dir="$(dirname "$(realpath "$0")")" # directory of where the script is
 
-jq 'del(."window-size", ."window-maximized", ."window-position", ."url") | .options.themes = [""]' config.json > config.json.build
+jq 'del(."window-size", ."window-maximized", ."window-position", ."url") | .options.themes = [""]' "$script_dir/config.json" > "$script_dir/config.json.build"
