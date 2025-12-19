7c5b1ce08f823acb15fd14d9461b1523442f9fd6
ponleou <dev.ponleousk@gmail.com>
2025-12-19 19:31:19 +1100

    autocommit: change in /home/ponleou/.dotfiles/stow-scripts/

--- stow-scripts/autosync-stow.sh
+++ stow-scripts/autosync-stow.sh
@@ -7,14 +7,6 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
 LAST_COMMIT_HASH=$(git rev-parse "$MERGE_BRANCH")
 
-git ls-files -z | xargs -0 -r git rm -f
-notify-send "Autosync is checking out $AUTO_BRANCH to $MERGE_BRANCH" "$(git checkout origin/$AUTO_BRANCH -- .)"
-git add -A
-commit_output=$(git commit -m "autosync: sync from $AUTO_BRANCH branch ($(date +'%d-%m-%Y %H:%M:%S'))")
-notify-send "Autosync is committing" "$commit_output"
-notify-send "Autosync is pushing" "$(git push origin $MERGE_BRANCH)"
-notify-send "Autosync completed"
-
 check_lock() {
   if [ -f "$SCRIPT_DIR/../tmp/$LOCK_FILE" ]; then
     notify-send "Autosync aborted" "$SCRIPT_DIR/../tmp/$LOCK_FILE exists"
@@ -40,8 +32,19 @@ safe_cd_tmp_dir() {
   cd "$TMP_DIR"
 }
 
-main() {
+squash_and_push_to_merge() {
+  git ls-files -z | xargs -0 -r git rm -f
+  notify-send "Autosync is checking out $AUTO_BRANCH to $MERGE_BRANCH" "$(git checkout origin/$AUTO_BRANCH -- .)"
+  git add -A
+  commit_output=$(git commit -m "autosync: sync from $AUTO_BRANCH branch ($(date +'%d-%m-%Y %H:%M:%S'))")
+  notify-send "Autosync is committing" "$commit_output"
+  notify-send "Autosync is pushing" "$(git push origin $MERGE_BRANCH)"
+  notify-send "Autosync completed"
+}
 
+main() {
+  check_lock
+  safe_cd_tmp_dir
 }
 
 main "$@"
