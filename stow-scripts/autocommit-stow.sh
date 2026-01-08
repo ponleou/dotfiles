90d75f66845f3389b5546b0cf1d5baca1c70d209
ponleou <dev.ponleousk@gmail.com>
2026-01-08 19:26:24 +1100

    autosync: sync from autocommit branch (08-01-2026 19:26:24)

--- stow-scripts/autocommit-stow.sh
+++ stow-scripts/autocommit-stow.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+COMMIT_TIMER_PID=""
 SYNC_TIMER_PID=""
+
+
+trigger_commit_after_wait() {
+  COMMIT_TIMER_FILE="$SCRIPT_DIR/../tmp/COMMIT_TIMER_PID"
+
+    # get PID from file
+  if [ -f "$COMMIT_TIMER_FILE" ]; then
+    COMMIT_TIMER_PID="$(cat "$COMMIT_TIMER_FILE")"
+  fi
+
+  # kill process if PID exist
+  if [ -n "$COMMIT_TIMER_PID" ]; then
+    kill "$COMMIT_TIMER_PID"
+    COMMIT_TIMER_PID="" 
+  fi
+
+    (
+    sleep 60 &
+    COMMIT_TIMER_PID=$!
+
+    # echo into file for next run to kill process
+    if [ -f "$COMMIT_TIMER_FILE" ]; then
+      echo "$COMMIT_TIMER_PID" > "$COMMIT_TIMER_FILE"
+    fi
+
+    # wait for sleep before running script
+    wait $COMMIT_TIMER_PID && bash "$SCRIPT_DIR/commit-stow.sh"
+  ) &
+}
 
 trigger_sync_after_idle() {
   SYNC_TIMER_FILE="$SCRIPT_DIR/../tmp/SYNC_TIMER_PID"
@@ -47,8 +77,10 @@ check_connection() {
 }
 
 export SCRIPT_DIR
+export -f trigger_commit_after_wait
 export -f trigger_sync_after_idle
 export -f trigger_hook
+export -f check_connection
 
 # it doesnt watch .git folder, and all files ending with .build
 inotifywait -q -m -r --exclude '/\.git($|/)|\.build$' \
@@ -58,4 +90,4 @@ inotifywait -q -m -r --exclude '/\.git($|/)|\.build$' \
   -e MOVED_TO \
   -e MOVED_FROM \
   -e MODIFY \
-  --format="trigger_hook && git add -A && git commit -m 'autocommit: change in %w' && git push && trigger_sync_after_idle" ~/.dotfiles | sh
+  --format="trigger_hook && check_connection && trigger_commit_after_wait && trigger_sync_after_idle" ~/.dotfiles | sh
---
+++ stow-scripts/commit-stow.sh
@@ -0,0 +1,3 @@
+git add -A
+git commit -m "autocommit on $(date +'%d-%m-%Y %H:%M:%S')"
+git push 
