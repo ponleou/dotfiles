a4c7b617c570dd05831749a5d7bc6cc8ce1f3007
ponleou <dev.ponleousk@gmail.com>
2026-02-23 23:41:12 +1100

    autocommit on 23-02-2026 23:41:12

--- stow-essentials.sh
+++ stow-essentials.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-packages="easyeffects GIMP mpv omz dolphin sway vesktop zen Code VSCodium ytm nvim"
+packages="alacritty Code dolphin easyeffects GIMP mpv nvim omz sway vesktop VSCodium ytm zen"
 build_packages="Code VSCodium ytm"
 
 script_dir="$(dirname "$(realpath "$0")")" # directory of where the script is
