ff8f32f20193ac84ce1b404b72238352d35d2405
ponleou <dev.ponleousk@gmail.com>
2026-02-27 15:03:21 +1100

    autosync: sync from autocommit branch (27-02-2026 15:03:21)

--- essentials/nvim/.config/nvim/init.lua
+++ essentials/nvim/.config/nvim/init.lua
@@ -6,9 +6,13 @@ vim.opt.expandtab = true
 vim.opt.softtabstop = 4
 
 -- Move lines up/down in visual mode
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move line down' })
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move line up' })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
 
 -- Move single line up/down in normal mode
-vim.keymap.set('n', '<A-j>', ':m .+1<CR>==', { desc = 'Move line down' })
-vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', { desc = 'Move line up' })
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+
+-- Comment line/visual selection with ctrl /
+vim.keymap.set("n", "<C-/>", "gcc", { remap = true })
+vim.keymap.set("v", "<C-/>", "gc", { remap = true })
