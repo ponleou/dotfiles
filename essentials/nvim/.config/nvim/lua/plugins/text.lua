26966e59f16fbe7852c3c27dabb06825996eed0c
ponleou <dev.ponleousk@gmail.com>
2026-04-01 16:39:42 +1100

    autocommit on 01-04-2026 16:39:42

--- essentials/nvim/.config/nvim/lua/plugins/text.lua
+++ essentials/nvim/.config/nvim/lua/plugins/text.lua
@@ -17,8 +17,8 @@ return {
 			vim.keymap.set("i", "<CR>", "<CR><cmd>AutolistNewBullet<cr>")
 			-- vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>")
 			-- vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
-			-- vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
-			-- vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
+			vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
+			vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
 
 			-- cycle list types with dot-repeat
 			-- vim.keymap.set("n", "<leader>cn", require("autolist").cycle_next_dr, { expr = true })
