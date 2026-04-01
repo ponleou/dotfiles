28ee4d367456ddda70ac343932d38c049cd9190c
ponleou <dev.ponleousk@gmail.com>
2026-04-01 16:38:15 +1100

    autocommit on 01-04-2026 16:38:15

--- essentials/nvim/.config/nvim/lua/plugins/text.lua
+++ essentials/nvim/.config/nvim/lua/plugins/text.lua
@@ -1,4 +1,5 @@
 return {
+	-- automatic insertion of lists in texts
 	{
 		"gaoDean/autolist.nvim",
 		ft = {
@@ -13,16 +14,15 @@ return {
 
 			vim.keymap.set("i", "<tab>", "<cmd>AutolistTab<cr>")
 			vim.keymap.set("i", "<s-tab>", "<cmd>AutolistShiftTab<cr>")
-			-- vim.keymap.set("i", "<c-t>", "<c-t><cmd>AutolistRecalculate<cr>") -- an example of using <c-t> to indent
 			vim.keymap.set("i", "<CR>", "<CR><cmd>AutolistNewBullet<cr>")
-			vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>")
-			vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
-			vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
-			vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
+			-- vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>")
+			-- vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
+			-- vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
+			-- vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
 
 			-- cycle list types with dot-repeat
-			vim.keymap.set("n", "<leader>cn", require("autolist").cycle_next_dr, { expr = true })
-			vim.keymap.set("n", "<leader>cp", require("autolist").cycle_prev_dr, { expr = true })
+			-- vim.keymap.set("n", "<leader>cn", require("autolist").cycle_next_dr, { expr = true })
+			-- vim.keymap.set("n", "<leader>cp", require("autolist").cycle_prev_dr, { expr = true })
 
 			-- if you don't want dot-repeat
 			-- vim.keymap.set("n", "<leader>cn", "<cmd>AutolistCycleNext<cr>")
