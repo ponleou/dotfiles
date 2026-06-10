ae6207decfa1cab58bc722bddff82ac34da7b3d1
ponleou <dev.ponleousk@gmail.com>
2026-06-10 14:27:04 +1000

    autocommit on 10-06-2026 14:27:04

--- essentials/nvim/.config/nvim/lua/plugins/colors.lua
+++
@@ -1,1 +0,0 @@
-../../base/colors.lua
--- essentials/sway/.config/sway/scripts/window-screenshot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-grim -g "$(swaymsg -t get_tree -r | jq -r '.. | (.nodes? // empty)[] | select(.focused) | .rect | "\(.x),\(.y) \(.width)x\(.height)"')" -l 0 -t ppm - | satty --filename - --fullscreen --copy-command wl-copy --early-exit --initial-tool crop --output-filename ~/Pictures/Screenshots/satty-$(date '+%Y%m%d-%H:%M:%S').png 
