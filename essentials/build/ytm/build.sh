36e30d18086426b6c88e2d76367b27667cd59877
ponleou <dev.ponleousk@gmail.com>
2026-06-10 14:04:37 +1000

    autosync: sync from autocommit branch (10-06-2026 14:04:36)

--- essentials/build/ytm/build.sh
+++ essentials/build/ytm/build.sh
@@ -5,4 +5,4 @@ script_dir="$(dirname "$(realpath "$0")")" # directory of where the script is
 cat $script_dir/config.json.build > $script_dir/config.json
 
 
-ln -sf $script_dir/config_build_hook.sh $script_dir/../../../stow-scripts/hooks/ytm_build_essentials.sh
+ln -sf $script_dir/config_build_hook.sh $script_dir/../../../service-scripts/hooks/ytm_build_essentials.sh
