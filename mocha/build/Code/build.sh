9e06a6f39df1c9a200338f5a7ab9f4b1b4c85a93
ponleou <dev.ponleousk@gmail.com>
2026-06-10 14:24:46 +1000

    autocommit on 10-06-2026 14:24:46

---
+++ mocha/build/Code/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# variables with "expbuild_" prefix are exported variables by the build function when called
+
+script_dir="$(dirname "$(realpath "$0")")" # directory of where the script is
+
+# modify the settings.json
+output=$(jq '."workbench.colorTheme" = "Catppuccin Mocha" | ."catppuccin.accentColor" = "'"$expbuild_accent"'"' "$script_dir/settings.json.build")
+
+if [ $? -eq 0 ]; then
+    echo "$output" > "$script_dir/settings.json"
+else
+    error=$(jq '."workbench.colorTheme" = "Catppuccin Mocha" | ."catppuccin.accentColor" = "'"$expbuild_accent"'"' "$script_dir/settings.json.build" 2>&1)
+    echo "Error $script_dir/$0: $error"
+fi
