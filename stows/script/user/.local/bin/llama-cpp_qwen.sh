6a832f16fbd1b943dcf12572d8955753c153f4a0
ponleou <dev.ponleousk@gmail.com>
2026-08-12 15:47:17 +1000

    autocommit on 12-08-2026 15:47:17

--- stows/script/user/.local/bin/llama-cpp_qwen.sh
+++ stows/script/user/.local/bin/llama-cpp_qwen.sh
@@ -1,3 +1,8 @@
 #!/bin/zsh
 
-nohup llama serve -hf unsloth/Qwen3.6-35B-A3B-MTP-GGUF:Q3_K_XL -ngl 999 --n-cpu-moe 30 -c 81920 -fa 1 -ctk q8_0 -ctv q8_0 --spec-type draft-mtp --spec-draft-n-max 2 -np 1 --temp 0.6 --top-p 0.95 --top-k 20 --min_p 0.0 --presence-penalty 0 --no-mmproj &
+log_file=/dev/null
+if [[ -n "$LLAMA_CACHE" ]]; then
+    log_file="$(basename "$0").log"    
+fi
+
+nohup llama serve -hf unsloth/Qwen3.6-35B-A3B-MTP-GGUF:Q3_K_XL -ngl 999 --n-cpu-moe 30 -c 81920 -fa 1 -ctk q8_0 -ctv q8_0 --spec-type draft-mtp --spec-draft-n-max 2 -np 1 --temp 0.6 --top-p 0.95 --top-k 20 --min_p 0.0 --presence-penalty 0 --no-mmproj 2>&1 | awk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' >> "$log_file" &
