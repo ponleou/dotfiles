62aa8b9c4ad213a5a819111b4ceb5ba29702c54c
ponleou <dev.ponleousk@gmail.com>
2026-08-17 13:11:28 +1000

    autocommit on 17-08-2026 13:11:28

--- stows/script/user/.local/bin/llama-cpp/llama-cpp_qwen.sh
+++ stows/script/user/.local/bin/llama-cpp/llama-cpp_qwen.sh
@@ -7,6 +7,6 @@ pid_file="$LLAMA_CACHE/pid"
 
 [[ -s "$pid_file" ]] && exit 1
 
-nohup llama serve -hf unsloth/Qwen3.6-35B-A3B-MTP-GGUF:Q3_K_XL -ngl 999 --n-cpu-moe 30 -c 81920 -fa 1 -ctk q8_0 -ctv q8_0 --spec-type draft-mtp --spec-draft-n-max 2 -np 1 --temp 0.6 --top-p 0.95 --top-k 20 --min_p 0.0 --presence-penalty 0 --no-mmproj --jinja > >(awk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0; fflush() }' >> "$log_file") 2>&1 &
+GGML_VK_VISIBLE_DEVICES=1 nohup llama serve -hf unsloth/Qwen3.6-35B-A3B-MTP-GGUF:Q3_K_XL -ngl 999 --n-cpu-moe 30 -c 81920 -fa 1 -ctk q8_0 -ctv q8_0 --spec-type draft-mtp --spec-draft-n-max 2 -np 1 --temp 0.6 --top-p 0.95 --top-k 20 --min_p 0.0 --presence-penalty 0 --no-mmproj --jinja > >(awk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0; fflush() }' >> "$log_file") 2>&1 &
 
 echo $! > "$pid_file"
