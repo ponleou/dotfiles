3349654816a2466c2449ff865e3f0e48ffd627e2
ponleou <dev.ponleousk@gmail.com>
2026-08-29 16:43:32 +1000

    autocommit on 29-08-2026 16:43:32

---
+++ stows/script/user/.local/bin/llama-cpp/llama-cpp_qwen-quality.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+[[ -z "$LLAMA_CACHE" ]] && exit 1
+
+log_file="$LLAMA_CACHE/$(basename "$0").log"    
+pid_file="$LLAMA_CACHE/pid"
+
+[[ -s "$pid_file" ]] && exit 1
+
+GGML_VK_VISIBLE_DEVICES=1 nohup llama serve -hf unsloth/Qwen3.6-35B-A3B-MTP-GGUF:Q4_K_S -ngl 999 --n-cpu-moe 32 -c 65536 -fa 1 -ctk f16 -ctv f16 --spec-type draft-mtp --spec-draft-n-max 2 -np 1 --temp 0.6 --top-p 0.95 --top-k 20 --min_p 0.0 --presence-penalty 0 --no-mmproj --jinja > >(awk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0; fflush() }' >> "$log_file") 2>&1 &
+
+echo $! > "$pid_file"
